87aac74d6e408f8cdb6da44457905a5b3775c4e6
Tom Kenny <tom@twome.name>
2019-04-25 20:50:43 +0800

    Update the page injector to allow for waitForDomContentLoaded settings

--- src/extension/injector.js
+++ src/extension/injector.js
@@ -1,11 +1,11 @@
+;(async function iifeForAwait(){
+
 // Options
 let patchHost = 'http://localhost:1917'
 let logInjections = true
 
 let last = arr => arr.reverse()[0]
 let fileExtension = path => last(path.split('.'))
-
-;(async function iifeForAwait(){
 
 // We need to encode to escape all the special URI characters
 let patchRequestPath = `${patchHost}/patches-for/${encodeURIComponent(location.href)}`
@@ -25,36 +25,53 @@ for (let patch of patches){
 		continue // Don't inject disabled patches
 	}
 	for (let asset of patch.assets){
-		
+
 		/*
 			If we wanted to insert the script as a `type="module"` _inline_ script (and wrap the body with, eg, a DOMContentLoaded listener), we could get the body text directly here.
 
-			// let asset = await (await fetch(`${patchHost}/${asset.fileUrl}`)).text()
-			// asset.body
+			let fileRequest = await fetch(`${patchHost}/${asset.fileUrl}`)
+			let assetBody = await fileRequest.text()
+			let invokeEl = document.createElement('script')
+			if (patch.options.waitForDomContentLoaded){
+				assetBody = `;document.addEventListener('DOMContentLoaded', function onDomContentLoaded(){\n${assetBody}\n});`
+			}
+			invokeEl.innerText = assetBody
+			invokeEl.setAttribute('type', 'module')
+			importInvocation = invokeEl
 		*/
-			
+
 		let extension = fileExtension(asset.fileUrl)
 		let fullAssetPath = `${patchHost}/${asset.fileUrl}`
 
-		let importInvocation
+		let invokeEl // The actual dependency invocation
 		if (extension === 'js'){
-			let el = document.createElement('script')
-			el.src = fullAssetPath
-			el.setAttribute('type', 'module')
-			importInvocation = el
+			invokeEl = document.createElement('script')
+			invokeEl.src = fullAssetPath
+			invokeEl.setAttribute('type', 'module')
 		} else if (extension === 'css'){
-			let el = document.createElement('link')
-			el.href = fullAssetPath
-			el.setAttribute('rel', 'stylesheet')
-			importInvocation = el
+			invokeEl = document.createElement('link')
+			invokeEl.href = fullAssetPath
+			invokeEl.setAttribute('rel', 'stylesheet')
 		} else {
-			throw Error(`Unknown extension ${extension}`)
+			console.error(Error(`Unknown extension ${extension}`))
+			continue
+		}
+		invokeEl.dataset.maneMatchList = patch.matchList.join(',')
+		invokeEl.dataset.maneId = patch.id
+
+		const inject = () => {
+			document.head.appendChild(invokeEl)
+			if (logInjections) console.info(`Mane: injected ${asset.fileUrl}`)
 		}
 
-		importInvocation.dataset.maneMatchList = patch.matchList.join(',')
-		importInvocation.dataset.maneId = patch.id
-		document.head.appendChild(importInvocation)
-		if (logInjections) console.info(`Mane: injected ${asset.fileUrl}`)
+		if (patch.options.waitForDomContentLoaded === true || document.readyState === 'complete'){
+			inject()
+		} else if (patch.options.waitForDomContentLoaded === 'full'){
+			// Wait for the page INCLUDING all dependent resources (styles, images etc) to be completely loaded
+			document.addEventListener('load', inject)
+		} else {
+			document.addEventListener('DOMContentLoaded', inject)
+		}
 	}
 }
 
